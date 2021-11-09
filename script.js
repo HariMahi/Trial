47f994ea5eff42daa6144c82c02ce516b8a152c0
Hari Pprasath <haripprasath1612@gmail.com>
2021-11-09 09:28:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,15 +7,14 @@ function readJSON(file) {
 }
 function load() {
    var res = readJSON('prod.json');
-        for(let product of res){
    var pi = document.getElementById('product_image');
-   pi.innerHTML = pi.innerHTML + ${product.image} + "<br>";
+   pi.innerHTML = pi.innerHTML + res[0].image + "<br>";
    var pn = document.getElementById('product_name');
-   pn.innerHTML = pn.innerHTML + ${product.name} + "<br>";
+   pn.innerHTML = pn.innerHTML + res[0].name + "<br>";
    var pp = document.getElementById('product_price');
-   pp.innerHTML = pp.innerHTML + ${product.price} + "<br>";
+   pp.innerHTML = pp.innerHTML + res[0].price + "<br>";
    var pd = document.getElementById('product_desc');
-   pd.innerHTML = pd.innerHTML + ${product.desc} + "<br>";
+   pd.innerHTML = pd.innerHTML + res[0].desc + "<br>";
    var pr = document.getElementById('product_rating');
-   pr.innerHTML = pr.innerHTML + ${product.rating} + "<br>";
+   pr.innerHTML = pr.innerHTML + res[0].rating + "<br>";
 }
