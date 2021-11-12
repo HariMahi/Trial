85ea161796922dcca39698287c0af87173198d80
Hari Pprasath <haripprasath1612@gmail.com>
2021-11-12 14:55:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,5 +22,5 @@ function load() {
    pr.innerHTML = pr.innerHTML + res[0].rating + "<br>";
 }
 function shopAlert() {
-  alert("Thank you for shopping with us! Kindly check your mail for further updates.");
+  alert("Thank you for shopping with us! Visit us soon again &#128525;.");
 }
